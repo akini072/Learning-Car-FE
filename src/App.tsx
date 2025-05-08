0845ff69fa11602048989f1949ff3fae0d1a9f4d
Akin <akin.ipek0505@gmail.com>
2025-05-09 02:45:36 +0300

    configuration

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Sidebar from './components/Sidebar';
+import './styles.scss';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+
+      {/* scss working test example */}
+      <div className="app">
+        <h1>Hello, world!</h1>
+      </div>
+    </>
+    
+  );
+};
+
+export default App;
